c860380fb0f01e62cdd8e7a2a01a2410b6ca5cf5
cartoonest74 <gyeng74@gmail.com>
2024-05-05 17:00:23 +0900

    gugudan >> webpack build 24/05/05 17:00

---
+++ gugudan/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDom = require("react-dom/client");
+
+const Gugudan = require("./gugudan");
+
+ReactDom.createRoot(document.querySelector("#root")).render(<Gugudan/>)
---
+++ gugudan/gugudan.jsx
@@ -0,0 +1,40 @@
+const React = require("react");
+const {component} = React;
+
+const Gugudan=()=>{
+    const [first,setFirst] = React.useState(Math.ceil(Math.random()*9));
+    const [second,setSecond] = React.useState(Math.ceil(Math.random()*9));
+    const [value,setValue] = React.useState('');
+    const [result,setResult] = React.useState(first * second);
+    const [comment,setCommnet] = React.useState(''); 
+    const inputRef = React.useRef(null);
+
+    const calc_result =()=>{
+        const answer_gugu = first * second;
+        inputRef.current.focus();
+        if(parseInt(answer_gugu) == value){
+            setFirst(Math.ceil(Math.random()*9)); 
+            setSecond(Math.ceil(Math.random()*9)); 
+            setValue(''); 
+            setCommnet('정답: '+ value);
+            return setResult(first * second)
+        }
+        return setCommnet('틀렸습니다');
+    }
+
+    const gugudan_onChange = (e)=>{
+        setValue(e.target.value);
+        return e.target.value;
+    }
+    console.log(111111)
+    return(
+        <React.Fragment>
+            <div>{first} x {second}</div>
+            <input ref={inputRef} type="number" onChange={gugudan_onChange}/>
+            <button type="button" onClick={calc_result}>click</button>
+            <div>{comment}</div>
+        </React.Fragment>
+    );
+}
+
+module.exports = Gugudan;
